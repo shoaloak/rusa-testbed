616907b812426263244bfaf1c87ded74d978c82e
Axel Koolhaas <axel.koolhaas@hotmail.com>
2023-12-26 13:54:36 +0100

    Added container network for db

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,12 +9,29 @@ readonly SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Load configuration
 source "${SCRIPT_PATH}/config.sh"
 
+# Create network
+docker network create testbed-network
+
 for vuln_no in "${TARGET_KEYS[@]}"; do
     target="${TARGETS[$vuln_no]}"
-
     print_line
+
+    echo "Starting database..."
+    docker run \
+        --rm \
+        --detach \
+        --name petclinic-db-postgresql \
+        --network testbed-network \
+        -e POSTGRES_DB=petclinic \
+        -e POSTGRES_PASSWORD=petclinic \
+        -v "$SCRIPT_PATH/resources/postgresql":/docker-entrypoint-initdb.d/ \
+        postgres:16.0
+
     echo "Executing container for vulnerability ${vuln_no} (${target})"
 
     # Execute RESTler
-    docker run -ti "testbed-restler-${vuln_no}"
+    docker run -ti --network testbed-network "testbed-restler-${vuln_no}"
+
+    # Stop database
+    docker stop petclinic-db-postgresql
 done
