49bd58e0f9d02b51e2ebe513d1d2c3c3d3c4311d
Axel Koolhaas <axel.koolhaas@hotmail.com>
2024-07-08 14:17:17 +0200

    Add: 3 iterations per vulnerability

--- scripts/run.sh
+++ scripts/run.sh
@@ -47,35 +47,39 @@ for vuln_no in "${TARGET_KEYS[@]}"; do
     printf "running 'testbed-restler-%s' for %s\nCPUs=%s\n\n" \
         "$vuln_no" "$target" "$CPU"
 
-    # Database
-    echo "Starting database..."
-    docker run \
-        --rm \
-        --detach \
-        --name "${DB}" \
-        --network testbed-network \
-        -e POSTGRES_DB=petclinic \
-        -e POSTGRES_PASSWORD=petclinic \
-        -v "${SCRIPT_PATH}/../resources/postgresql":/docker-entrypoint-initdb.d/ \
-        postgres:16.0
-    # sleep 4 # usually takes 2s, but wait 4s to be safe
-    until check_postgres; do
-        echo "Waiting for PostgreSQL to be ready..."
-        sleep 1
+    for i in {1..3}; do
+        echo "Iteration $i for vulnerability ${vuln_no} (${target})"
+
+        # Database
+        echo "Starting database..."
+        docker run \
+            --rm \
+            --detach \
+            --name "${DB}" \
+            --network testbed-network \
+            -e POSTGRES_DB=petclinic \
+            -e POSTGRES_PASSWORD=petclinic \
+            -v "${SCRIPT_PATH}/../resources/postgresql":/docker-entrypoint-initdb.d/ \
+            postgres:16.0
+        # sleep 4 # usually takes 2s, but wait 4s to be safe
+        until check_postgres; do
+            echo "Waiting for PostgreSQL to be ready..."
+            sleep 1
+        done
+
+        # Server
+        echo "Executing container for vulnerability ${vuln_no} (${target})"
+        docker run -ti \
+            --cpus="${CPU}" --cpuset-cpus=0-$((CPU - 1)) \
+            --network testbed-network \
+            --volume "${RESULT_PATH}/${vuln_no}/${i}":/host_result_folder \
+            -e DB="jdbc:postgresql://${DB}:5432/petclinic" \
+            -e TIMEOUT_SEC=30 \
+            "testbed-restler-${vuln_no}"
+
+        # Stop database
+        docker stop petclinic-db-postgresql
     done
-
-    # Server
-    echo "Executing container for vulnerability ${vuln_no} (${target})"
-    docker run -ti \
-        --cpus="${CPU}" --cpuset-cpus=0-$((CPU - 1)) \
-        --network testbed-network \
-        --volume "${RESULT_PATH}/${vuln_no}":/host_result_folder \
-        -e DB="jdbc:postgresql://${DB}:5432/petclinic" \
-        -e TIMEOUT_SEC=30 \
-        "testbed-restler-${vuln_no}"
-
-    # Stop database
-    docker stop petclinic-db-postgresql
 done
 
 # Remove network
